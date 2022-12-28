3d1f91d49a3e2ba60be4660e318ba28df702e4b7
xx <god_xu@aliyun.com>
2022-12-29 04:03:26 +0800

    First Commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author XX
+ * @data 2022/12/29 3:49
+ * @email godxu_forever@163.com
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,Git");
+    }
+}
